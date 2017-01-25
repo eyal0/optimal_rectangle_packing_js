e69d91cb79ffc78049a1133a2443588860255b5f
Eyal Soha <eyalsoha@gmail.com>
2017-01-25 21:57:38 +0200

    typo

--- optimal_rectangle_packing.js
+++ optimal_rectangle_packing.js
@@ -286,8 +286,8 @@ var ORP = {
     }
     var insert_result = insert_all_rectangles(rectangles, max_rectangle_height);
     var area = insert_result.area;
-    var placements = insert_results.placements;
-    var min_delta_height = insert_results.min_delta_height;
+    var placements = insert_result.placements;
+    var min_delta_height = insert_result.min_delta_height;
     console.log(area.grid_to_string(50,50,"  ", function(x) { return x.name; }));
     console.log("Empty column at " + find_first_empty_column(area));
   }
